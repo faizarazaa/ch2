1652403829a5307572230e1063b7ec209d96e551
Faiza Razaa <125462568+faizarazaa@users.noreply.github.com>
2023-05-30 17:47:38 +0500

    Add files via upload

---
+++ assignment q7/app.js
@@ -0,0 +1,22 @@
+// var username = "faiza";
+// alert( "username is "+username)
+
+// var myname = "faiza Raza";
+// alert("my name is  " + myname)
+
+// var message = "hello world";
+// alert(message)
+
+// var myname = "faiza";
+// alert(myname)
+// var age = "28";
+// alert(age)
+// var subject = "mobile app developement";
+// alert(subject)
+
+
+// var pizza="PIZZA \n PIZZ \n PIZ \n PI\n P"
+// alert (pizza)
+
+var email = "example@example.com"
+alert ("My email address is" + " " + email)
